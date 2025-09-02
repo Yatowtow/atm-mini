49097d8249211aa3b6a018fc845a306d424d6ee7
Jefferson Tresvalles <jeffersontresvalles41@gmail.com>
2025-09-02 23:44:54 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+let balance = 5000;
+
+
+function updateScreen(message) {
+    document.getElementById("screen").innerText = message;
+}
+
+function getAmount() {
+    return parseFloat(document.getElementById("amount").value);
+}
+
+function checkBalance() {
+    updateScreen(`Your balance is $${balance}`);
+}
+
+function deposit ()  {
+    let amount = getAmount();
+    if(amount > 0) {1
+        balance += amount;
+      updateScreen("Deposited $" + amount + " | New balance: $" + balance);
+    } else {
+        updateScreen("Invalid deposit amount");
+    }
+    document.getElementById(amount).value = "";
+}
+
+function withdraw() {
+    let amount = getAmount();
+    if (amount > 0 && amount <= balance) {
+        balance -= amount;
+        updateScreen("Withdrew $" + amount + " | New balance: $" + balance);
+    }
+    else {
+        updateScreen("Invalid or insuffienct funds");
+    }
+    document.getElementById(amount).value = "";
+}
+
+function exitATM() {
+    updateScreen("Thank you for using the atm machine ninja");
+    document.getElementById("amount").disabled = true;
+}
+
+
+
+
+
+
